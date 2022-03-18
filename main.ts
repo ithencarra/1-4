465904e79ddb7bbe68c82456bdebc7ce21b3b79b
ithencarra <101406398+ithencarra@users.noreply.github.com>
2022-03-18 13:48:05 +0100

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,12 @@
+input.onButtonPressed(Button.B, function () {
+	
+})
+if (input.buttonIsPressed(Button.A)) {
+    basic.showIcon(IconNames.Happy)
+}
+if (input.buttonIsPressed(Button.B)) {
+    basic.showIcon(IconNames.Sad)
+}
+basic.forever(function () {
+	
+})
